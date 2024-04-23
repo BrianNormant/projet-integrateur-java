0f65dccf65e30d5ddf5f0ece5166638c4881bcaa
BrianNixDesktop <briannormant@gmail.com>
2024-04-23 16:57:19 -0400

    Fix flemme bug

--- app/src/main/java/main/Main.java
+++ app/src/main/java/main/Main.java
@@ -22,23 +22,22 @@ import io.vavr.control.Either;
 
 public class Main extends JFrame {
 
-	
+
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private CarteInterface carte = new CarteInterface(getX(),getY(),getWidth(),getHeight());
-	private LoginInterface login = new LoginInterface(getX(),getY(),getWidth(),getHeight());
-	private ReservationInterface reservation = new ReservationInterface (getX(),getY(),getWidth(),getHeight());
-	private StationInterface station = new StationInterface(getX(),getY(),getWidth(),getHeight());
-	private RailInterface rail = new RailInterface(getX(),getY(),getWidth(),getHeight());
-	private AllTrainInterface train= new AllTrainInterface(getX(),getY(),getWidth(),getHeight());
-	private RechercheInterface recherche= new RechercheInterface(getX(),getY(),getWidth(),getHeight());
+	private CarteInterface carte;
+	private LoginInterface login;
+	private ReservationInterface reservation;
 	private static String token;
 	private static String nom;
 	private static String pwd;
-	private boolean autorisation;
-	
+	private StationInterface station;
+	private RailInterface rail;
+	private RechercheInterface recherche;
+	private AllTrainInterface train;
 
-	private static boolean modeFlemme = false;
+
+	private static boolean modeFlemme = true;
 
 	static {
 		if (modeFlemme) {
@@ -48,21 +47,189 @@ public class Main extends JFrame {
 		}
 	}
 
-	
+
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		
+
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					Main frame = new Main();
-					frame.carte.setJFrame(frame);
-					frame.setVisible(true);
 					frame.login.requestFocusInWindow();
+					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public void initAfterLogin() {
+		carte = new CarteInterface(getX(),getY(),getWidth(),getHeight());
+		reservation = new ReservationInterface (getX(),getY(),getWidth(),getHeight());
+		station = new StationInterface(getX(),getY(),getWidth(),getHeight());
+		rail = new RailInterface(getX(),getY(),getWidth(),getHeight());
+		train = new AllTrainInterface(getX(),getY(),getWidth(),getHeight());
+		recherche = new RechercheInterface(getX(),getY(),getWidth(),getHeight());
+		setBounds(100, 100, 1010, 600);
+		login.setVisible(false);
+		carte.setVisible(true);
+		setContentPane(carte);
+		carte.requestFocusInWindow();
+		carte.addPropertyChangeListener(new PropertyChangeListener(){
+			public void propertyChange(PropertyChangeEvent evt) {
+				switch (evt.getPropertyName()) {
+					case "passerReservation":
+						setBounds(100, 100, 1000, 580);
+						carte.setVisible(false);
+						reservation.setVisible(true);
+						setContentPane(reservation);
+						reservation.requestFocusInWindow();
+						break;
+
+					case "passerStation":
+						setBounds(100, 100, 1000, 400);
+						int id=(Integer)evt.getNewValue();
+						station.setToken(token);
+						station.setId(id);
+						carte.setVisible(false);
+						station.setVisible(true);
+						setContentPane(station);
+						station.requestFocusInWindow();
+						break;
+
+					case "passerRail":
+						int idRail=(Integer)evt.getNewValue();
+						setBounds(100, 100, 1000, 700);
+						rail.setId(idRail);
+						carte.setVisible(false);
+						rail.setVisible(true);
+						setContentPane(rail);
+						rail.requestFocusInWindow();
+						break;
+
+					case "logout":
+						setBounds(100, 100, 730, 450);
+						carte.setVisible(false);
+						login.setVisible(true);
+						setContentPane(login);
+						login.requestFocusInWindow();
+						break;
+
+					case "passerRecherche":
+						carte.setVisible(false);
+						recherche.setVisible(true);
+						setContentPane(recherche);
+						recherche.requestFocusInWindow();
+						break;
+
+					case "passerTrain":
+						carte.setVisible(false);
+						train.setVisible(true);
+						setContentPane(train);
+						train.requestFocusInWindow();
+						train.setFalse();
+						train.refresh();
+						break;
+
+					case "TrainInterface":
+						var idTrain = (Integer) evt.getNewValue();
+						carte.setVisible(false);
+						var t = Train.createOrGet(idTrain);
+						var trainI = new TrainInterface(t);
+						trainI.addPropertyChangeListener(evt2 -> {
+							System.out.println(evt2.getPropertyName());
+							switch(evt2.getPropertyName()) {
+								case "back" -> {
+	System.out.printf("handled protected String name;\n");
+	setBounds(100, 100, 1010, 600);
+	trainI.setVisible(false);
+	carte.setVisible(true);
+	setContentPane(carte);
+	carte.requestFocusInWindow();
+								}
+							}
+						});
+						setContentPane(trainI);
+						trainI.requestFocusInWindow();
+						pack();
+						break;
+				}
+			}
+		}
+		);
+
+		reservation.addPropertyChangeListener(new PropertyChangeListener(){
+			public void propertyChange(PropertyChangeEvent evt) {
+				switch (evt.getPropertyName()) {
+					case "back":
+						setBounds(100, 100, 1010, 600);
+						reservation.setVisible(false);
+						carte.setVisible(true);
+						setContentPane(carte);
+						carte.requestFocusInWindow();
+						break;
+				}
+			}
+		}
+		);
+
+		station.addPropertyChangeListener(new PropertyChangeListener(){
+			public void propertyChange(PropertyChangeEvent evt) {
+				switch (evt.getPropertyName()) {
+					case "back":
+						setBounds(100, 100, 1010, 600);
+						station.setVisible(false);
+						carte.setVisible(true);
+						setContentPane(carte);
+						carte.requestFocusInWindow();
+						break;
+				}
+			}
+		}
+		);
+		rail.addPropertyChangeListener(new PropertyChangeListener(){
+			public void propertyChange(PropertyChangeEvent evt) {
+				switch (evt.getPropertyName()) {
+					case "back":
+						setBounds(100, 100, 1010, 600);
+						rail.setVisible(false);
+						carte.setVisible(true);
+						setContentPane(carte);
+						carte.requestFocusInWindow();
+						break;
+				}
+			}
+		}
+		);
+
+		train.addPropertyChangeListener(new PropertyChangeListener(){
+			public void propertyChange(PropertyChangeEvent evt) {
+				switch (evt.getPropertyName()) {
+					case "back":
+						setBounds(100, 100, 1010, 600);
+						train.setVisible(false);
+						carte.setVisible(true);
+						setContentPane(carte);
+						carte.requestFocusInWindow();
+						break;
+				}
+			}
+		}
+		);
+
+		recherche.addPropertyChangeListener(new PropertyChangeListener(){
+			public void propertyChange(PropertyChangeEvent evt) {
+				switch (evt.getPropertyName()) {
+					case "back":
+						setBounds(100, 100, 1100, 600);
+						recherche.setVisible(false);
+						carte.setVisible(true);
+						setContentPane(carte);
+						carte.requestFocusInWindow();
+						break;
 				}
 			}
 		});
@@ -76,191 +243,40 @@ public class Main extends JFrame {
 		setBounds(100, 100, 730, 450);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		
+
+		login = new LoginInterface(getX(),getY(),getWidth(),getHeight());
+
 		setContentPane(login);
 		if(modeFlemme) {
 			flemme();
 		}
-		
+
+		var This = this;
+
 		login.addPropertyChangeListener(new PropertyChangeListener(){
 			public void propertyChange(PropertyChangeEvent evt) {
 				switch (evt.getPropertyName()) {
-				case "passerCarte":
-					
-					setNom(login.getNom());
-					String password = new String(login.getPwd());
-					setPwd(password);
-					token = setToken();
-					if(token == null) {
-						login.mauvaisIdentifiant();
-						break;
-					}
-					if(!nom.equals("admin")) {
-						login.nonAutorise();
-						break;
-					}
-					setBounds(100, 100, 1010, 600);
-					login.setVisible(false);
-					carte.setVisible(true);
-					setContentPane(carte);
-					carte.requestFocusInWindow();
-					break;
-				}
-			}
-		}
-		);
-		
-		carte.addPropertyChangeListener(new PropertyChangeListener(){
-			public void propertyChange(PropertyChangeEvent evt) {
-				switch (evt.getPropertyName()) {
-				case "passerReservation":
-					setBounds(100, 100, 1000, 580);
-					carte.setVisible(false);
-					reservation.setVisible(true);
-					setContentPane(reservation);
-					reservation.requestFocusInWindow();
-					break;
-					
-				case "passerStation":
-					setBounds(100, 100, 1000, 400);
-					int id=(Integer)evt.getNewValue();
-					station.setToken(token);
-					station.setId(id);
-					carte.setVisible(false);
-					station.setVisible(true);
-					setContentPane(station);
-					station.requestFocusInWindow();
-					break;
-					
-				case "passerRail":
-					int idRail=(Integer)evt.getNewValue();
-					setBounds(100, 100, 1000, 700);
-					rail.setId(idRail);
-					carte.setVisible(false);
-					rail.setVisible(true);
-					setContentPane(rail);
-					rail.requestFocusInWindow();
-					break;
-					
-				case "logout":
-					setBounds(100, 100, 730, 450);
-					carte.setVisible(false);
-					login.setVisible(true);
-					setContentPane(login);
-					login.requestFocusInWindow();
-					break;
-					
-				case "passerRecherche":
-					carte.setVisible(false);
-					recherche.setVisible(true);
-					setContentPane(recherche);
-					recherche.requestFocusInWindow();
-					break;
-					
-				case "passerTrain":
-					carte.setVisible(false);
-					train.setVisible(true);
-					setContentPane(train);
-					train.requestFocusInWindow();
-					train.setFalse();
-					train.refresh();
-					break;
-					
-				case "TrainInterface":
-					var idTrain = (Integer) evt.getNewValue();
-					carte.setVisible(false);
-					var t = Train.createOrGet(idTrain);
-					var trainI = new TrainInterface(t);
-					trainI.addPropertyChangeListener(evt2 -> {
-						System.out.println(evt2.getPropertyName());
-						switch(evt2.getPropertyName()) {
-						case "back" -> {
-							System.out.printf("handled protected String name;\n");
-							setBounds(100, 100, 1010, 600);
-							trainI.setVisible(false);
-							carte.setVisible(true);
-							setContentPane(carte);
-							carte.requestFocusInWindow();
+					case "passerCarte":
+
+						setNom(login.getNom());
+						String password = new String(login.getPwd());
+						setPwd(password);
+						token = setToken();
+						if(token == null) {
+							login.mauvaisIdentifiant();
+							break;
 						}
+						if(!nom.equals("admin")) {
+							login.nonAutorise();
+							break;
 						}
-					});
-					setContentPane(trainI);
-					trainI.requestFocusInWindow();
-					pack();
-					break;
-				}
-			}
-		}
-		);
-		reservation.addPropertyChangeListener(new PropertyChangeListener(){
-			public void propertyChange(PropertyChangeEvent evt) {
-				switch (evt.getPropertyName()) {
-				case "back":
-					setBounds(100, 100, 1010, 600);
-					reservation.setVisible(false);
-					carte.setVisible(true);
-					setContentPane(carte);
-					carte.requestFocusInWindow();
-					break;
-				}
-			}
-		}
-		);
-		
-		station.addPropertyChangeListener(new PropertyChangeListener(){
-			public void propertyChange(PropertyChangeEvent evt) {
-				switch (evt.getPropertyName()) {
-				case "back":
-					setBounds(100, 100, 1010, 600);
-					station.setVisible(false);
-					carte.setVisible(true);
-					setContentPane(carte);
-					carte.requestFocusInWindow();
-					break;
-				}
-			}
-		}
-		);
-		rail.addPropertyChangeListener(new PropertyChangeListener(){
-			public void propertyChange(PropertyChangeEvent evt) {
-				switch (evt.getPropertyName()) {
-				case "back":
-					setBounds(100, 100, 1010, 600);
-					rail.setVisible(false);
-					carte.setVisible(true);
-					setContentPane(carte);
-					carte.requestFocusInWindow();
-					break;
-				}
-			}
-		}
-		);
-		
-		train.addPropertyChangeListener(new PropertyChangeListener(){
-			public void propertyChange(PropertyChangeEvent evt) {
-				switch (evt.getPropertyName()) {
-				case "back":
-					setBounds(100, 100, 1010, 600);
-					train.setVisible(false);
-					carte.setVisible(true);
-					setContentPane(carte);
-					carte.requestFocusInWindow();
-					break;
-				}
-			}
-		}
-		);
-		
-		recherche.addPropertyChangeListener(new PropertyChangeListener(){
-			public void propertyChange(PropertyChangeEvent evt) {
-				switch (evt.getPropertyName()) {
-				case "back":
-					setBounds(100, 100, 1100, 600);
-					recherche.setVisible(false);
-					carte.setVisible(true);
-					setContentPane(carte);
-					carte.requestFocusInWindow();
-					break;
+
+						initAfterLogin();
+
+						carte.setJFrame(This);
+						setVisible(true);
+
+
 				}
 			}
 		}
@@ -286,7 +302,7 @@ public class Main extends JFrame {
 	public static String setToken() {
 		Either<String, LoginError> result = RestApi.requestLogin(nom, pwd);
 		System.out.println(result);
-		
+
 		if(result.isLeft()) {
 			return result.getLeft();
 		}else {
@@ -296,8 +312,10 @@ public class Main extends JFrame {
 
 	}
 
-	
+
 	private void flemme() {
+		initAfterLogin();
+
 		setBounds(100, 100, 1010, 600);
 		login.setVisible(false);
 		carte.setVisible(true);
